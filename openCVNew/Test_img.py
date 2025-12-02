a20aea8d15c7ed888c4c947af79833c79f9d74e7
SiweiLiao3 <siweiliao3-c@my.cityu.edu.hk>
2025-12-02 17:37:24 +0800

    delete useless args

--- openCVNew/Test_img.py
+++ openCVNew/Test_img.py
@@ -10,26 +10,16 @@ import math
 import cv2
 from PIL import Image
 
-parser = argparse.ArgumentParser(description='PSMNet')
-parser.add_argument('--loadmodel', default='./trained/pretrained_model_KITTI2015.tar',
-                    help='loading model')
+parser = argparse.ArgumentParser(description='SGBM')
 parser.add_argument('--leftimg', default= './StereoMatchingTestings/Art/view1.png',
                     help='load model')
 parser.add_argument('--rightimg', default= './StereoMatchingTestings/Art/view5.png',
                     help='load model')                                      
-parser.add_argument('--model', default='stackhourglass',
-                    help='select model')
 parser.add_argument('--maxdisp', type=int, default=192,
                     help='maxium disparity')
-parser.add_argument('--no_cuda', action='store_true', default=False,
-                    help='enables CUDA training')
-parser.add_argument('--seed', type=int, default=1, metavar='S',
-                    help='random seed (default: 1)')
 parser.add_argument('--device', choices=['auto', 'cpu', 'cuda', 'mps'], default='auto',
                     help='device to run on: auto, cpu, cuda, or mps (Apple Silicon)')
 args = parser.parse_args()
-args.cuda = not args.no_cuda and torch.cuda.is_available()
-
 
 def resolve_device(pref: str) -> torch.device:
     if pref == 'cpu':
@@ -143,51 +133,16 @@ def post_process_disparity(pred_disp, top_pad, right_pad, parent_dir):
 
 
 def main():
-    normal_mean_var = {'mean': [0.485, 0.456, 0.406],
-                        'std': [0.229, 0.224, 0.225]}
-    infer_transform = transforms.Compose([transforms.ToTensor(),
-                                            transforms.Normalize(**normal_mean_var)])    
+    test_dirs = ["Art", "Dolls", "Reindeer"] 
 
-    imgL_o = Image.open(args.leftimg).convert('RGB')
-    imgR_o = Image.open(args.rightimg).convert('RGB')
-
-    imgL = infer_transform(imgL_o)
-    imgR = infer_transform(imgR_o) 
-    
-
-    # pad to width and hight to 16 times
-    if imgL.shape[1] % 16 != 0:
-        times = imgL.shape[1]//16       
-        top_pad = (times+1)*16 -imgL.shape[1]
-    else:
-        top_pad = 0
-
-    if imgL.shape[2] % 16 != 0:
-        times = imgL.shape[2]//16                       
-        right_pad = (times+1)*16-imgL.shape[2]
-    else:
-        right_pad = 0    
-
-    imgL = F.pad(imgL,(0,right_pad, top_pad,0)).unsqueeze(0)
-    imgR = F.pad(imgR,(0,right_pad, top_pad,0)).unsqueeze(0)
+    imgL, imgR, top_pad, right_pad = pre_process_disparity(args.leftimg, args.rightimg)
 
     start_time = time.time()
     pred_disp = compute_SGBM(imgL,imgR)
     print('time = %.2f' %(time.time() - start_time))
 
-    
-    if top_pad !=0 and right_pad != 0:
-        img = pred_disp[top_pad:,:-right_pad]
-    elif top_pad ==0 and right_pad != 0:
-        img = pred_disp[:,:-right_pad]
-    elif top_pad !=0 and right_pad == 0:
-        img = pred_disp[top_pad:,:]
-    else:
-        img = pred_disp
-    
-    img = (img*256).astype('uint16')
-    img = Image.fromarray(img)
-    img.save(f'Test_disparity_2.png')
+    parent_dir = args.leftimg.split('/')[-2]
+    post_process_disparity(pred_disp, top_pad, right_pad, parent_dir)
 
 if __name__ == '__main__':
    main()
